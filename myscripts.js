70dc2ccb5419bedc06915cd62f68f83adfa65929
codzitie <gmkrishnakumar333@gmail.com>
2023-10-04 12:37:33 +0530

    update without styles

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,6 @@
+let color = 'red';
+let click = true;
+
 function createBoard(size){
     let board = document.querySelector('.board');
     let squares = board.querySelectorAll('div');
@@ -8,11 +11,8 @@ function createBoard(size){
     amount = size*size;
     for (i = 0; i<amount; i++){
         let square = document.createElement('div');
-        square.addEventListener('mousemove',()=> {
-            square.style.backgroundColor = 'pink';
-        });
+        square.addEventListener('mousemove',colorSquare);
         square.style.backgroundColor = 'grey';
-        square.style.border = 'white';
         board.insertAdjacentElement('beforeend',square);
     }
 };
@@ -27,3 +27,41 @@ function changeSize(input){
     }
     
 };
+
+function colorSquare(){
+    if (click) {
+        if(color === 'random'){
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        }else{
+            this.style.backgroundColor = color;
+        };
+    };
+        
+            
+};
+
+function changeColor(choice){
+    color = choice;
+}
+
+function resetBoard(){
+    click = !false;
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div)=>div.remove());
+    // squares.forEach((div) => (div.style.backgroundColor = 'grey'));
+    value = document.querySelector('input').value;
+    createBoard(value);
+}
+
+document.querySelector('body').addEventListener('click',(e)=>{
+    if (e.target.tagname != 'BUTTON'){
+        click = !click;
+        if(click){
+            document.querySelector('.mode').textContent = 'Mode: Coloring'
+        }else{
+            document.querySelector('.mode').textContent = 'Mode: Not Coloring'
+
+        }
+    }
+})
